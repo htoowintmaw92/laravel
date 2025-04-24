ef677b75beec2c870345cacd377c0d9803a5dc60
hhtoo <kg-htoo@infonic.co.jp>
2025-04-25 04:39:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['error' => 'Unhealthy'], 400);
 });
 
 Route::get('/test', function () {
